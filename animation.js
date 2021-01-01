4ca5365d6f4fbbd9ca89b8513b93d5141aada32b
essawy99 <61217404+essawy99@users.noreply.github.com>
2021-01-01 17:13:48 -0600

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,8 +1,14 @@
+var dir = 0;
+var ball = new User();
+var cannon = new Cannon(200,200);
+
 view.onFrame = function(event) {
 ball.update(dir);
 dir = 0;
 cannon.update();
-if(total_distance > 2000){
+
+  
+ if(total_distance > 2000){
   var plane_2 = new Enemy_Plane();
   planeArray.push(plane_2)
   total_distance = 0
@@ -13,4 +19,21 @@ if(total_distance > 2000){
   }
 } //Update all current planes
 total_distance += 20; //total distance goes up by 20 each time even after reset
+  
+ 
+
 }
+
+
+
+tool.onKeyDown = function(event) {
+    if(event.key == 'a' || event.key == 'left') {
+        dir = -1;
+                          
+    }
+
+    if(event.key == 'd' || event.key == 'right') {
+        dir = 1;           
+    }       
+}
+
