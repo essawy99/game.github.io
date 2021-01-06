ee666f01d28bdc603022549cacb871bb7b4154fc
essawy99 <61217404+essawy99@users.noreply.github.com>
2021-01-06 00:11:23 -0600

    idk

--- Ball&ship.js
+++ Ball&ship.js
@@ -6,31 +6,58 @@ class Cannon {
         this.x = xLoc;
         this.y = yLoc;
         this.direction = (1/4)* Math.PI;
-        this.angle = this.direction;
         this.cannon = new Path.Circle({
             center: new paper.Point(this.x, this.y),
             radius: 3* wUnit,
             fillColor: 'black'
         });
+        this.x_vel = this.velocity * Math.cos(this.direction);
+        this.y_vel = this.velocity * Math.sin(this.direction);
     }
 
     update() {
-        var x_vel = this.velocity * Math.cos(this.direction);
-        var y_vel = this.velocity * Math.sin(this.direction);
         
-        this.x += x_vel;
-        this.y += y_vel;
+        this.x += this.x_vel;
+        this.y += this.y_vel;
         this.cannon.position.x = this.x;
         this.cannon.position.y = this.y;
         /* if(this.x < 0 || this.x > w) {
             dir += Math.PI; 
         } */
     }
-    collision(angle){
-        var x1 = this.velocity * Math.cos(this.direction); //x velocity
-        var x2 = this.velocity * Math.sin(this.direction); //y velocity
-        this.direction = 2*(angle)-this.angle
-        this.angle = angle
+    collision(ang){ //collision method input degrees
+        //Method 2 - Takes in angle as argument
+        var angle = (ang*Math.PI) / 180;
+        console.log(angle)
+        var n = { //Vector based off of angle 
+            x: Math.sin(angle),
+            y: -Math.cos(angle)
+        }
+        var v = { //Ball vector
+            x: this.x_vel,
+            y: this.y_vel
+        }
+        var d = 2 * this.dot(v, n);
+        this.x_vel -= d * n.x;
+        this.y_vel -= d * n.y;
+        this.x += this.x_vel;
+        this.y += this.y_vel;
+        this.cannon.position.x = this.x;
+        this.cannon.position.y = this.y;
+    }
+    dot(v1, v2) {
+        return v1.x * v2.x + v1.y * v2.y
+      }
+    check(){ //function to check collision with top sides and bottom
+        if(this.x <= 0){
+            this.collision(90);
+        }else if(this.x >= document.getElementById("myCanvas").width){
+            this.collision(270);
+        }else if(this.y <= 0){
+            this.collision(360);
+        }else if(this.y >= document.getElementById("myCanvas").height){
+            this.collision(180);
+        }
     }
 }
         
