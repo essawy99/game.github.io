f58316ee359f0111c66694753c76dd087f6bf251
yellowflash3454 <yellowflash3454@gmail.com>
2021-01-06 21:10:34 -0600

    Fixed ball and force field collisions

--- Ball&ship.js
+++ Ball&ship.js
@@ -52,6 +52,10 @@ class Cannon {
     //array1 is ship array array2 is plane array
     check(user,array1,array2){ //function to check collision with top sides and bottom
         //An array of all intersections between ball and forcefield
+        var clone_ball = this.cannon.clone();
+        clone_ball.visible = false;
+        clone_ball.position.x += this.x_vel;
+        clone_ball.position.y += this.y_vel;
         if(this.x <= 0){
             this.collision(90);
         }else if(this.x >= w){
@@ -60,14 +64,22 @@ class Cannon {
             this.collision(360);
         }else if(this.y >= h){
             this.collision(180);
-        }else if(this.cannon.intersects(user.arc)){ //Check if ball and arc touch
+        }else if(clone_ball.intersects(user.arc)){ //Check if ball and arc touch
             //If ball and arc touch get intersections and use
             //first point as collision
-            var intersections =  this.cannon.getIntersections(user.arc);
-            console.log("Intersections list: " + intersections)
-            var tangent = intersections[0].tangent;
-            console.log("Angle of tangent: " + (tangent.angle+360)%360);
-            this.collision((tangent.angle+360)%360); //Change angle position by 360 degrees
+            var intersections =  clone_ball.getIntersections(user.arc);
+            var length = intersections.length;
+            var tangent_line = new paper.Path();
+            var i;
+            for(i = 0;i<length;i++){
+                tangent_line.add(new paper.Point(intersections[i].point));
+                console.log("intersection point: " + intersections[i].point)
+            }
+            console.log("Tangent_line: " + tangent_line);
+            console.log("length: " + tangent_line.length);
+            var tangent = tangent_line.getTangentAt(tangent_line.length);
+            console.log("tangent: " + tangent)
+            this.collision(tangent.angle);
         }
         }
 }
