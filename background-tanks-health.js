d40d75f549fd54d7e07764f417119dfa8f029c7b
Dheva <dheva70@gmail.com>
2021-01-07 16:31:55 -0600

    Added health animation and added some light code commenting

--- background-tanks-health.js
+++ background-tanks-health.js
@@ -1,54 +1,37 @@
-// IN ORDER TO IMPLEMENT, REPLACE BUILDING SCRIPT W THIS ONE
-// REMOVE BUILDING DECLARATION IN ANIMATION AND DECLARE 
-// BACKGROUND HEALTH AND TANKS(parameter: how many tanks desired) SEPERATELY
-
-// NOTE, EITHER SHRINK OR MOVE BOAT UP SMALL PORTION
-
-//-----------------------------------------------------------------------------
-
-
-
-
-
-class Background { //DHEVAA TODO: Implement wave animation on shore
+class Background { //TODO(Dheva): Implement wave animation on shore
 	constructor() {
-		const rect = new Path.Rectangle(new Point(0, 0), new Size(w, h));
-		rect.fillColor = new Color("#42adf5");
+		const water = new Path.Rectangle(new Point(0, 0), new Size(w, h)); // Water portion
+		water.fillColor = new Color("#42adf5");
 		
-		const rect2 = new Path.Rectangle(new Point(0, 7 * h / 8), new Size(w, h));
-		rect2.fillColor = new Color("#ebd8a0");
+		const beach = new Path.Rectangle(new Point(0, 7 * h / 8), new Size(w, h)); // Beach/Sand portion
+		beach.fillColor = new Color("#ebd8a0");
 	}
 }
 
-class Tanks { //DHEVA TODO: Implement explosion animation on damage
-	static tankArray = [];
+class Tanks { //TODO(Dheva): REPLACE WITH NEW DESIGN
+	static tankArray = []; //Static array holding all generated tank objects
 	
-	constructor(amount) {
+	constructor(amount) { //Calculates spacing and generates scaled tanks 
 		this._numTanks = amount;
-		this.initDraw();
-	}
-	
-	get numTanks() { return this._numTanks; }
-	
-	initDraw() {
+		
 		const tankSpacing = w / (this._numTanks + 1);
 		const tankUnit = (2 * h / 64);
 		for(let i = 1; tankSpacing * i < w; i++) {
-			Tanks.tankArray.push(this.drawTank(new Point((tankSpacing*i)-(tankUnit/2), (57*h/64)), tankUnit));
+			Tanks.tankArray.push(this.drawTank(new Point((tankSpacing*i)-(tankUnit/2), (57*h/64)), tankUnit)); //Pushes to static array
 		}
 	}
 	
-	drawTank (point, unit) {
+	drawTank (point, unit) { //Draws individual tanks
 		let tank = new Path.Rectangle(point, new Size(unit, 2 * unit));
-		console.log(point.x);
-		console.log(point.y)
 		tank.fillColor = new Color("#0b5703");
 		tank.strokeColor = new Color("#074001");
 		return tank
 	}
+	
+	get numTanks() { return this._numTanks; }
 }
 
-class Health { //DHEVA TODO: Implement bar decrease animation on damage
+class Health {
 	constructor() {
 		this._health = 100;
 		
@@ -56,9 +39,13 @@ class Health { //DHEVA TODO: Implement bar decrease animation on damage
 		this._healthBar.fillColor = new Color("#db2316");
 	}
 	
-	get health() { return this._health; }
-	
 	takeDamage(damagePercent) {
-		this._health -= damage;
+		this._health -= damagePercent;
+		this._healthBar.tween({ //Tween animation to reduce the size of the healthbar based on damage taken
+		'segments[3].point': ['-=', {x: damagePercent*w/100, y: 0}],
+			'segments[2].point': ['-=', {x: damagePercent*w/100, y: 0}]
+		}, 500);
 	}
+	
+	get health() { return this._health; }
 }
