0948841159c7ee2528cf2f4d7bece2c579e05463
yellowflash3454 <yellowflash3454@gmail.com>
2021-01-13 22:02:32 -0600

    Periodic push

--- controller.js
+++ controller.js
@@ -33,7 +33,16 @@ function delete_all(){
 /*
    TODO (DHEVA): Function should call your function
    to show the campaign home */
-function campaign_home(){
+function campaign_home(environment,level){
+   // Remove home first
+   endHome();
+   console.log("FINISH")
+}
+
+/*
+   TODO (DHEVA): Function should call your function
+   to show the survival home */
+function survival_home(){
    // Remove home first
    endHome();
    console.log("FINISH")
@@ -46,19 +55,36 @@ function campaign_home(){
                GAME LOOP
 
 */
+let gameStatus = 0; //1-> Game is running, 0-> Game is paused
 
-while(true){ //CHANGE CONDITION ONCE IM CLEAR ON WHAT IT IS
-   let gameStatus = 0; //1-> Game is running, 0-> Game is paused
+//----------------------------------------------------------------- paper.js set up
+paper.install(window);
+var canvas = document.getElementById('myCanvas');
+paper.setup(canvas);
 
-   //----------------------------------------------------------------- paper.js set up
-   paper.install(window);
-   var canvas = document.getElementById('myCanvas');
-   paper.setup(canvas);
+//Start home
+start_home();
 
-   //Start home
-   start_home();
+// Variable that stores game_mode
+var game_mode;
 
-   /*
-   Upon button click start home should vanish and either
+// Environemnt variable
+var environment;
+
+/*
+   Upon button click on home page either
    campaign_home() or survival_home() should be called */
-}
+
+
+/*
+   Two event listeners that check if either survival is chosen 
+   or if campaign is chosen
+*/
+document.getElementById("survival").addEventListener('click',(event) =>{
+   game_mode = "survival";
+   survival_home(environment);
+});
+document.getElementById("campaign").addEventListener('click',(event) =>{
+   game_mode = "campaign"
+   survival_home(environment,);
+});
