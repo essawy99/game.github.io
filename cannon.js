aaab6de38c1a421b1703ecec2b7e069f71c41969
yellowflash3454 <yellowflash3454@gmail.com>
2021-01-17 22:31:06 -0600

    Fixed ball passing through forcefield bug:

--- cannon.js
+++ cannon.js
@@ -4,22 +4,22 @@ class CannonBalls {
         this.array = [];
         this.array.push(new CannonBall(500 *wUnit,500 *hUnit, this.home));
     }
-    
+
     /* Iterates through each live cannonball, checks for
     collisions and updates location */
     update(user,ships,planes) {
-        
+
         // iterates through each cannonball
         for(var i = 0; i < this.array.length; i++) {
-            
+
             var cannonBall = this.array[i]
             // make sure a ball exists in that location
             if(cannonBall != null) {
                 // update the ball using update function
                 // if the ball is destroyed it will return true
                 // triggering code to nullify that array position
-                
-                if(cannonBall.update(user,ships,planes)) {      
+
+                if(cannonBall.update(user,ships,planes)) {
                     this.shipArray[i] = null
                 }
             }
@@ -37,16 +37,16 @@ class CannonBalls {
         }
         return true;
     }
-   
+
     /* iterates through ship array and adds new
-    cannon to a null location. if there are no null 
-    locations it pushes a cannonBall to a new location 
-    Function constructs cannonBall at the location of the 
+    cannon to a null location. if there are no null
+    locations it pushes a cannonBall to a new location
+    Function constructs cannonBall at the location of the
     cannon*/
     addBall(user) {
         for(var i = 0; i < this.array.length; i++) {
             if(this.array[i] == null) {
-                this.array[i] = 
+                this.array[i] =
                 new CannonBall(user.arc.position.x, user.arc.position.y, this.home);
                 return;
             }
@@ -84,20 +84,20 @@ class CannonBall {
     update(user,ships,planes) {
         // slightly accelerates ball each frame
         this.velocity += .001 * hUnit;
-        // checks for colisions that would change direction  
+        // checks for colisions that would change direction
         this.check(user,ships,planes);
         // update location of ball based on x and y velocity
         this.cannon.position.x += this.x_vel;
         this.cannon.position.y += this.y_vel;
     }
-    
-    /* Method bounces the ball off the angle inputted into the function 
+
+    /* Method bounces the ball off the angle inputted into the function
        using vector multiplication */
-    collision(ang){ 
+    collision(ang){
         var angle = (ang*Math.PI) / 180; // calculates angle in radians
-        
-        
-        var n = {                        // Vector based off of angle 
+
+
+        var n = {                        // Vector based off of angle
             x: Math.sin(angle),
             y: -Math.cos(angle)
         }
@@ -114,42 +114,42 @@ class CannonBall {
     dot(v1, v2) {                        //Dot multiply function
         return v1.x * v2.x + v1.y * v2.y
     }
-    
+
     /* function checks for colisons between ball and user/ships/planes/walls
         function automatically updates accordingly */
     check(user,ships,planes){
 
         // x and y positions that determine where ball would be on next frame
-        var xFuture = this.cannon.position.x + this.x_vel; 
+        var xFuture = this.cannon.position.x + this.x_vel;
         var yFuture = this.cannon.position.y + this.y_vel;
-    
+
         //create a line going from current cannonball to future cannonball
-        var line = new Path.Line(new Point(this.cannon.position.x,this.cannon.position.y), 
+        var line = new Path.Line(new Point(this.cannon.position.x,this.cannon.position.y),
                                  new Point(xFuture, yFuture));
-                                 
+
         // if were not in home screen check for arc collisions
         if(!this.home) {
             this.checkArcCollisons(user,line);
         }
         //Check for collisions with ships and planes
-        // using general CheckEnemyCollisoins fucntion 
+        // using general CheckEnemyCollisoins fucntion
         this.checkEnemyCollisions(ships, line, user);
         this.checkEnemyCollisions(planes, line, user);
-        
-        
-        
+
+
+
         line.remove(); // Remove line to save memory
 
         // checks for wall collisions and returns false
         // if the ball is out of bounds
         return this.checkWallCollisions();
-    }      
-    
-    /* Checks fo wall collisons returns false if the ball 
-    falls through bottom of screen. In home screen ball 
+    }
+
+    /* Checks fo wall collisons returns false if the ball
+    falls through bottom of screen. In home screen ball
     bounces off of bottom */
     checkWallCollisions() {
-        // series of if statements for bounces of walls 
+        // series of if statements for bounces of walls
         if(this.cannon.position.x <= 0){
             this.collision(90);
         }
@@ -162,38 +162,77 @@ class CannonBall {
         // if home screen ball bounces off bottom
         if(this.cannon.position.y >= h && this.home) {
 			this.collision(180);
-        } 
+        }
         // in this situation we kill the ball
         else if(this.cannon.position.y >= h){
-            // Remove cannon ball when it hits bottom of screen 
+            // Remove cannon ball when it hits bottom of screen
             this.cannon.remove();
             // Set ball to 'dead'
             this.alive = false;
             return false;
         }
     }
-    
+
     /* if not in home screen we bounce ball off of arc. If the ball
         and arc makes contact the ball direction changes to the normal
         vector of the point of intersection between the two */
     checkArcCollisons(user, line) {
-        
 		if(line.intersects(user.arc)){ //Check if ball and arc touch
-				
+
 			// Get first intersection between line and arc
 			var point = line.getIntersections(user.arc)[0].point;
-			
+
 			// Calculate offset from arc
 			var offset = user.arc.getOffsetOf(point);
 			// Use offset to Get normal
 			var normal = user.arc.getNormalAt(offset);
-		
+
 			// Set x and y velocity according to new angle
 			this.x_vel = normal.x * this.velocity;
 			this.y_vel = normal.y * this.velocity;
-		}
+        }else{ //Implement new collision
+            var xFuture = this.cannon.position.x + this.x_vel;
+            var yFuture = this.cannon.position.y + this.y_vel;
+            //Create new shape based off of line and line + user.previousMove
+
+            var shape = new Path();
+
+            //Add current cannon position
+            shape.add(this.cannon.position.x,this.cannon.position.y);
+            //Add future cannon position
+            shape.add(xFuture,yFuture);
+            // Current cannon position + previousMove of user
+            shape.add(this.cannon.position.x+user.previousMove,this.cannon.position.y);
+            // Future cannon position + previousMove of user
+            shape.add(xFuture+user.previousMove,yFuture);
+
+            //Shape settings
+            shape.visible = false;
+            shape.closed = true;
+            //Check if user.arc intersects with this shape
+            if(shape.intersects(user.arc)){
+                // Offset value depending on if left or right previous user movement
+                var offset;
+                if(user.previousMove < 0){
+                    // If left
+                    offset = user.arc.length * (3/4);
+                }else{
+                    // If right
+                    offset = user.arc.length * (1/4);
+                }
+                // Use offset to Get normal
+                var normal = user.arc.getNormalAt(offset);
+
+                // Set x and y velocity according to new angle
+                this.x_vel = normal.x * this.velocity;
+                this.y_vel = normal.y * this.velocity;
+
+                //Remove shape
+                shape.remove();
+            }
+        }
 	}
-     
+
     checkEnemyCollisions(container, line, user) {
 
         // array containin ships/planes
@@ -205,7 +244,7 @@ class CannonBall {
                 var object = array[i].body
                 // check for intersection between outside of ship/plane and line
                 if(line.intersects(object.path0)){
-					
+
                     // get first point of intersection
                     var point = line.getIntersections(object.path0)[0].point;
 
@@ -213,19 +252,19 @@ class CannonBall {
                     var offset = object.path0.getOffsetOf(point);
                     // Use offset to Get tangent
                     var tanPoint = object.path0.getTangentAt(offset)
-                    
+
                     // colide ball with tangent angle
                     this.collision(tanPoint.angle);
 
                     // damage ship/plane i
                     if(!this.home) {
                         container.takeDamage(100, i, user);
-                    }    
+                    }
                 }
             }
         }
     }
-    
+
     /* Simple function to remove ball */
     remove() {
         this.cannon.remove();
--- user.js
+++ user.js
@@ -1,150 +1,156 @@
-     
+
 class User {
     constructor() {
-        //Initialize user score and coins
-        this.score = 0;
-        this.coins = 0;
-        
-        // y location of user on screen
-        this.userYLoc = 814 * hUnit;
-        
-        // size and speed attributes can be used to change entire User
-        this.size = 40 * wUnit;
-        
-        this.speed = 20 * wUnit;
-
-        //Can be changed once implementation of collision works
-        this.x_vel = 0;
-        this.y_vel = 0;
-
-        //forcefield properties in terms of size
-        this.forcefieldWidth = this.size;
-        this.forcefieldHeight = this.forcefieldWidth / 2;
-        
-        //ship Body properties in terms of size
-        this.shipTop = this.userYLoc + (this.size /2);
-        this.shipHeight = this.size / 5;
-        this.shipWidth = this.size * (4/5);
-        this.shipCorner = this.size * (3/5);
-
-        // points associated with forcefield
-        this.from = new paper.Point
-            (center - this.forcefieldWidth, this.userYLoc);   // left
-        this.through = new paper.Point
-            (center, this.userYLoc - this.forcefieldHeight ); // top
-        this.to = new paper.Point
-            (center + this.forcefieldWidth, this.userYLoc);   //right
-                
-        // forcefield path consisting of arc and connecting line
-        this.arc = new paper.Path.Arc(this.from, this.through, this.to);
-        this.connect = new paper.Path();
-        this.connect.add(this.from);
-        this.connect.add(this.to);
-
-        // create path associated with the ship
-        this.shipBody = new paper.Path()
-                
-        /* Ship points written in terms of properties */
-        //left bound
-        this.shipBody.add(new paper.Point
-            (center - this.shipWidth, this.shipTop)); 
-        //top left corner
-        this.shipBody.add(new paper.Point
-            (center - this.shipCorner, this.shipTop - this.shipHeight)); 
-        //top right corner
-        this.shipBody.add(new paper.Point
-            (center + this.shipCorner, this.shipTop - this.shipHeight)); 
-        //right bound
-        this.shipBody.add(new paper.Point
-            (center + this.shipWidth, this.shipTop)); 
-        // bottom right corner
-            this.shipBody.add(new paper.Point
-            (center + this.shipCorner, this.shipTop + this.shipHeight)); 
-        // bottom left corner
-        this.shipBody.add(new paper.Point
-            (center - this.shipCorner, this.shipTop + this.shipHeight)); 
-        
-        // style the ship body
-        this.shipBody.closed = true;
-        this.shipBody.fillColor = 'grey';
-      
-        // style the forcefield
-        /* this.arc.fullySelected = true; */
-        this.arc.strokeColor = 'blue';
-        /* this.connect.strokeColor = 'blue'; */
-        this.arc.strokeWidth = 3;
-        /* this.connect.strokeWidth = 2;
-        this.arc.fillColor = 'cyan'; */
-        this.sp = -1;
-
-        this.generateElectricity();
+       //Initialize user score and coins
+       this.score = 0;
+       this.coins = 0;
+    
+       // y location of user on screen
+       this.userYLoc = 814 * hUnit;
+    
+    
+       // Previous Move
+    
+       this.previousMove;
+       // size and speed attributes can be used to change entire User
+       this.size = 40 * wUnit;
+    
+       this.speed = 20 * wUnit;
+    
+       //Can be changed once implementation of collision works
+       this.x_vel = 0;
+       this.y_vel = 0;
+    
+       //forcefield properties in terms of size
+       this.forcefieldWidth = this.size;
+       this.forcefieldHeight = this.forcefieldWidth / 2;
+    
+       //ship Body properties in terms of size
+       this.shipTop = this.userYLoc + (this.size /2);
+       this.shipHeight = this.size / 5;
+       this.shipWidth = this.size * (4/5);
+       this.shipCorner = this.size * (3/5);
+    
+       // points associated with forcefield
+       this.from = new paper.Point
+           (center - this.forcefieldWidth, this.userYLoc);   // left
+       this.through = new paper.Point
+           (center, this.userYLoc - this.forcefieldHeight ); // top
+       this.to = new paper.Point
+           (center + this.forcefieldWidth, this.userYLoc);   //right
+    
+       // forcefield path consisting of arc and connecting line
+       this.arc = new paper.Path.Arc(this.from, this.through, this.to);
+       this.connect = new paper.Path();
+       this.connect.add(this.from);
+       this.connect.add(this.to);
+    
+       // create path associated with the ship
+       this.shipBody = new paper.Path()
+    
+       /* Ship points written in terms of properties */
+       //left bound
+       this.shipBody.add(new paper.Point
+           (center - this.shipWidth, this.shipTop));
+       //top left corner
+       this.shipBody.add(new paper.Point
+           (center - this.shipCorner, this.shipTop - this.shipHeight));
+       //top right corner
+       this.shipBody.add(new paper.Point
+           (center + this.shipCorner, this.shipTop - this.shipHeight));
+       //right bound
+       this.shipBody.add(new paper.Point
+           (center + this.shipWidth, this.shipTop));
+       // bottom right corner
+           this.shipBody.add(new paper.Point
+           (center + this.shipCorner, this.shipTop + this.shipHeight));
+       // bottom left corner
+       this.shipBody.add(new paper.Point
+           (center - this.shipCorner, this.shipTop + this.shipHeight));
+    
+       // style the ship body
+       this.shipBody.closed = true;
+       this.shipBody.fillColor = 'grey';
+    
+       // style the forcefield
+       /* this.arc.fullySelected = true; */
+       this.arc.strokeColor = 'blue';
+       /* this.connect.strokeColor = 'blue'; */
+       this.arc.strokeWidth = 3;
+       /* this.connect.strokeWidth = 2;
+       this.arc.fillColor = 'cyan'; */
+       this.sp = -1;
+    
+       this.generateElectricity();
     }
-
+    
     generateElectricity(xPos) {
-        this.sp++;
-        //console.log('made');
-        if(this.sp % 2 == 0) {
-
-        var lUnit = this.arc.length /10
-        var l = 0;
-        
-        
-        this.elecSource = {x : this.shipBody.position.x - 5 * wUnit, y: this.shipBody.position.y};
-        this.elec = new Path();
-        this.elec.add(new Point(this.through.x, this.through.y));
-        this.elec.strokeColor = 'blue';
-        this.elec.strokeWidth = 2;
-        this.sp = 0;
-        this.arc.strokeWidth = 2;
-        
-        
-        for(var i = 0; i < 4; i++) {
-            var random = (Math.random() * 10 * wUnit) - 5 * wUnit;
-            var off = (this.through.y - this.to.y) / 4
-            this.elec.add(new Point(this.through.x + random, this.through.y - off * i));
-        }
-        this.elec.add(this.elecSource.x, this.elecSource.y);
+       this.sp++;
+       //console.log('made');
+       if(this.sp % 2 == 0) {
+    
+       var lUnit = this.arc.length /10
+       var l = 0;
+    
+    
+       this.elecSource = {x : this.shipBody.position.x - 5 * wUnit, y: this.shipBody.position.y};
+       this.elec = new Path();
+       this.elec.add(new Point(this.through.x, this.through.y));
+       this.elec.strokeColor = 'blue';
+       this.elec.strokeWidth = 2;
+       this.sp = 0;
+       this.arc.strokeWidth = 2;
+    
+    
+       for(var i = 0; i < 4; i++) {
+           var random = (Math.random() * 10 * wUnit) - 5 * wUnit;
+           var off = (this.through.y - this.to.y) / 4
+           this.elec.add(new Point(this.through.x + random, this.through.y - off * i));
+       }
+       this.elec.add(this.elecSource.x, this.elecSource.y);
     }
     }
-
+    
     update(point) {
-        this.elec.remove();
-        this.arc.strokeWidth = 0;
-        this.generateElectricity(point.x);
-        this.arc.position.x = point.x;
-        this.connect.position.x = point.x;
-        this.shipBody.position.x = point.x;
-
-        
-    } 
+       //Store previous move
+       this.previousMove = this.arc.position.x - point.x; //Double check!
+       this.elec.remove();
+       this.arc.strokeWidth = 0;
+       this.generateElectricity(point.x);
+       this.arc.position.x = point.x;
+       this.connect.position.x = point.x;
+       this.shipBody.position.x = point.x;
+    
+    }
     spend_coins(cost){ // decrease coins upon spending
-        this.coins -= cost; 
+       this.coins -= cost;
     }
     scoreUpdate(points){ // Update score and coins
-        this.score += points;
-        this.coins += points;
-        document.getElementById("scoreDisplay").innerHTML = "Score: " + this.score;
-		document.getElementById("moneyDisplay").innerHTML = "Money: " + this.coins;
-		
-		if(this.coins >= 400) {
-			document.getElementById("buy").style.color = "#d63d22";
-			document.getElementById("buy").style.borderColor = "#d63d22";
-		} else if(this.coins < 400) {
-			document.getElementById("buy").style.borderColor = "grey";
-			document.getElementById("buy").style.color = "grey";
-		}
+       this.score += points;
+       this.coins += points;
+       document.getElementById("scoreDisplay").innerHTML = "Score: " + this.score;
+    document.getElementById("moneyDisplay").innerHTML = "Money: " + this.coins;
+    
+    if(this.coins >= 400) {
+     document.getElementById("buy").style.color = "#d63d22";
+     document.getElementById("buy").style.borderColor = "#d63d22";
+    } else if(this.coins < 400) {
+     document.getElementById("buy").style.borderColor = "grey";
+     document.getElementById("buy").style.color = "grey";
+    }
     }
     update_balls(num){ // Update number of balls
-        this.num_balls += num;
+       this.num_balls += num;
     }
     get_balls(){ //Return number of balls
-        return this.num_balls;
+       return this.num_balls;
     }
-	
-	deleteAll() { //Does this delete everything?
-		this.shipBody.remove();
-		this.arc.remove();
-		this.connect.remove();
-		this.elec.remove();
-	}
-}
+    
+    deleteAll() { //Does this delete everything?
+    this.shipBody.remove();
+    this.arc.remove();
+    this.connect.remove();
+    this.elec.remove();
+    }
+    }
+    
