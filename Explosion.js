5bf8396c1759d52e78cfb5d1ef8d62ea11472637
Dheva <dheva70@gmail.com>
2021-01-18 20:58:21 -0600

    Renamed and commented Crater.js (now Explosion.js)

--- Explosion.js
+++ Explosion.js
@@ -1,20 +1,19 @@
 /* Object to create crater (explosion) objects and store it's attributes */
 class Crater {
 	constructor(point) {
-		let explosion = this._generateExplosion(point, h/40, h/20);
+		let explosion = this._generateExplosion(point, h/40, h/20); //Generates an explosion pattern where the convex points lie on a circle of radius h/20 and the concave points on a circle of radius h/40
 		
-		explosion.tween({scaling: 0.001}, {scaling: 1}, 100).then(function() {
+		explosion.tween({scaling: 0.001}, {scaling: 1}, 100).then(function() { //Tween animation, first expands from near zero size, the shinks to zero
 			explosion.tween({scaling: 1}, {scaling: 0}, 100);
 		});
 	}
 
 	
-	get center() { return this.crator.center; }
-	get craterObjects() { return Crater.craterArray; }
+	get center() { return this.crator.center; } //Returns the center of the graphic
 	
 	//-----------------------------------------------------------------
     /* Function to generate explosion */
-	_generateExplosion(point, innerR, outerR) {
+	_generateExplosion(point, innerR, outerR) { //Function uses the input values and basic trig to calculate the desired points on the circles 
 		const xVarOuter = Math.cos(Math.PI/4)*outerR;
 		const yVarOuter = Math.sin(Math.PI/4)*outerR;
 		
